cd351770360142175a48d74fa84e0d008763d50b
Saly Moubarak <saly@juke4me.com>
2021-12-21 17:23:27 +0100

    add model

---
+++ RedisOM/Model/CustomerRedis.cs
@@ -0,0 +1,18 @@
+﻿using Redis.OM.Modeling;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedisOM.Model
+{
+    [Document]
+    public class CustomerRedis
+    {
+        [Indexed(Sortable = true)] public string FirstName { get; set; }
+        [Indexed(Sortable = true)] public string LastName { get; set; }
+        [Indexed(Sortable = true)] public string Email { get; set; }
+        [Indexed(Sortable = true)] public int Age { get; set; }
+    }
+}
